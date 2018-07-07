18138ba6b0b41226aabc80e2757ef4fc698e8337
aersoz87 <ers.ahmet@gmail.com>
2018-07-07 11:32:29 +0300

    core data fonksiyonları ve keyboardun acilim kapanması eklendi

--- gadabout/ViewController.swift
+++ gadabout/ViewController.swift
@@ -8,8 +8,100 @@
 
 import UIKit
 import Parse
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var enterPassLabel: UILabel!
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signIn: UIButton!
+    
+    @IBOutlet weak var haveAnAccountLabel: UILabel!
+    
+    @IBOutlet weak var logIn: UIButton!
+    
+    var isEnglish = true
+    
+    func SaveLanguageSelection(English: Bool) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "UserLanguageSelection")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    result.setValue(English, forKey: "isEnglish")
+                    
+                    do {
+                        try context.save()
+                        print("Saved")
+                    } catch {
+                        print("There was error in Core Data save")
+                    }
+                    
+                }
+            }
+            else {
+                let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+                
+                newUser.setValue(English, forKey: "isEnglish")
+                
+                do {
+                    try context.save()
+                    print("Saved")
+                } catch {
+                    print("There was error in Core Data save")
+                }
+            }
+        } catch {
+            
+         // Catch code
+            
+        }
+    }
+    
+    func SetLabelLanguages(English: Bool) {
+        
+        print("Set Label Languages: \(English)")
+        
+        if English {
+            
+            
+            enterPassLabel.text = "Enter your email and password!"
+            usernameTextField.placeholder = "Username"
+            passwordTextField.placeholder = "Password"
+            logIn.setTitle("Login", for: [])
+            signIn.setTitle("Sign In", for: [])
+            haveAnAccountLabel.text = "Do not have an account?"
+            
+        } else {
+            print("inside else")
+            
+            
+            enterPassLabel.text = "Email ve şifrenizi giriniz!"
+            usernameTextField.placeholder = "Kullanıcı adı"
+            passwordTextField.placeholder = "Şifre"
+            logIn.setTitle("Giriş", for: [])
+            signIn.setTitle("Kaydol", for: [])
+            haveAnAccountLabel.text = "Kaydolmadınız mı?"
+            
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,16 +125,35 @@ class ViewController: UIViewController {
     
     
     @IBAction func SetTurkish(_ sender: Any) {
+        
+        SetLabelLanguages(English: isEnglish)
+        SaveLanguageSelection(English: isEnglish)
+
     }
     
     
     @IBAction func SetEnglish(_ sender: Any) {
+        
+        isEnglish = true
+        
+        SetLabelLanguages(English: isEnglish)
+        SaveLanguageSelection(English: isEnglish)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
 
 
 }
