87002d3f8a9b67d097ce8811ead7111f6369017f
aersoz87 <ers.ahmet@gmail.com>
2018-10-18 20:42:34 +0300

    foods sayfası tamamlandı

--- gadabout/foodsTableViewCell.swift
+++ gadabout/foodsTableViewCell.swift
@@ -35,6 +35,15 @@ class foodsTableViewCell: UITableViewCell {
     
     @IBOutlet weak var option4: UILabel!
     
+    @IBOutlet weak var detailsButton: UIButton!
+    
+    @IBOutlet weak var detailTextInfo: UITextView!
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    
+    @IBOutlet weak var toDoListButton: UIButton!
+    
+    
     var option1Marked = false
     var option2Marked = false
     var option3Marked = false
@@ -44,7 +53,7 @@ class foodsTableViewCell: UITableViewCell {
     
     var checkedOption = -1
     
-    var delegate: placesTableViewCellDelegate?
+    var delegate: foodsTableViewCellDelegate?
     
     @IBAction func markOption1Tapped(_ sender: Any) {
         if option1Marked {
@@ -71,12 +80,117 @@ class foodsTableViewCell: UITableViewCell {
             checkedOption = 1
         }
         print("Option 1 marked: \(option1Marked)")
-        //delegate?.didAlternativeSelected( sender: self,selectedIndex: checkedOption)
+        delegate?.didAlternativeSelected( sender: self,selectedIndex: checkedOption)
+
+    }
+    
+    @IBAction func markOption2Tapped(_ sender: Any) {
+        if option2Marked {
+            markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+            option1Marked = false
+            option2Marked = false
+            option3Marked = false
+            option4Marked = false
+            checkedOption = 0
+            
+        }
+        else {
+            markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption2.setImage(UIImage(named: "check.png"), for: [])
+            markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+            option1Marked = false
+            option2Marked = true
+            option3Marked = false
+            option4Marked = false
+            checkedOption = 2
+        }
+        delegate?.didAlternativeSelected( sender: self,selectedIndex: checkedOption)
 
     }
     
     
+    @IBAction func markOption3Tapped(_ sender: Any) {
+        
+        if option3Marked {
+            markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+            option1Marked = false
+            option2Marked = false
+            option3Marked = false
+            option4Marked = false
+            checkedOption = 0
+            
+        }
+        else {
+            markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption3.setImage(UIImage(named: "check.png"), for: [])
+            markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+            option1Marked = false
+            option2Marked = false
+            option3Marked = true
+            option4Marked = false
+            checkedOption = 3
+        }
+        delegate?.didAlternativeSelected( sender: self,selectedIndex: checkedOption)
 
+    }
+    
+
+    @IBAction func markOption4Tapped(_ sender: Any) {
+        if option4Marked {
+            markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+            option1Marked = false
+            option2Marked = false
+            option3Marked = false
+            option4Marked = false
+            checkedOption = 0
+            
+        }
+        else {
+            markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+            markOption4.setImage(UIImage(named: "check.png"), for: [])
+            option1Marked = false
+            option2Marked = false
+            option3Marked = false
+            option4Marked = true
+            checkedOption = 4
+        }
+        //print("\(checkedOption)")
+        delegate?.didAlternativeSelected( sender: self,selectedIndex: checkedOption)
+
+    }
+    
+    
+    @IBAction func detailButtonTapped(_ sender: Any) {
+        
+        if showDetail == false {
+            showDetail = true
+        }
+        else {
+            showDetail = false
+        }
+        
+        delegate?.isDetailButtonTapped(sender: self)
+    }
+    
+    
+    @IBAction func addToDoListTapped(_ sender: Any) {
+        delegate?.appendToDoList(sender: self)
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- gadabout/foodsTableViewController.swift
+++ gadabout/foodsTableViewController.swift
@@ -10,12 +10,73 @@ import UIKit
 import Parse
 import CoreData
 
-class foodsTableViewController: UITableViewController {
+class foodsTableViewController: UITableViewController, foodsTableViewCellDelegate {
 
+    var option1 = [String]()
+    var option2 = [String]()
+    var option3 = [String]()
+    var option4 = [String]()
+    var descriptionEng = [String]()
+    var descriptionTr = [String]()
+    var correctAnswer = [String]()
+    var imageFile = [PFFile]()
+    var imageArr = [UIImage]()
+    var showDetail = [Bool]()
+    var questionSeenBefore = [String]()
+    var questionCompleted = [String]()
+    
+    var detailCellRow: Int = 0
+    
+    var answer:[Int] = []
+    var questionNo:[Int] = []
+    
+    var isCompleted = false
+    var status: Int = -1
+    var selected: Int = -1
+    var mustBeSelected: Int = -1
+    
+    var detailText: String = ""
+
+    
+    
     
     @IBAction func backTapped(_ sender: Any) {
         performSegue(withIdentifier: "foodsBackSegue", sender: self)
         
+    }
+    
+    
+    @IBAction func didCompleteTapped(_ sender: Any) {
+        let sectionNo = 0
+        
+        if isCompleted == false {
+            isCompleted = true
+            let nofQuestions = correctAnswer.count
+            
+            
+            for rowNo in 0...nofQuestions-1 {
+                
+                let rowToSelect: IndexPath = IndexPath(row: rowNo, section: sectionNo)
+                self.tableView.reloadRows(at: [rowToSelect], with: .fade)
+            }
+        }
+        
+        for question in questionCompleted {
+            let needToSaveData = PFObject(className: "foodsCoveredBefore")
+            needToSaveData["userId"] = PFUser.current()?.objectId
+            needToSaveData["questionId"] = question
+            needToSaveData.saveInBackground(block: { (success, error) in
+                
+                if success {
+                    print("Current user is saved in place record")
+                }
+                else {
+                    print("Could not saved")
+                }
+                
+            })
+            
+        }
     }
     
     
@@ -27,7 +88,58 @@ class foodsTableViewController: UITableViewController {
         self.tableView.allowsSelection = true
         
         self.tableView.rowHeight = 380
-
+        
+        //questionSeenBefore.removeAll()
+        
+        let questionCoveredQuery = PFQuery(className: "foodsCoveredBefore")
+        questionCoveredQuery.whereKey("userId", equalTo: PFUser.current()?.objectId)
+        questionCoveredQuery.findObjectsInBackground { (objects, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else {
+                if let places = objects {
+                    for place in places {
+                        print("\(place["questionId"])")
+                        self.questionSeenBefore.append(place["questionId"] as! String)
+                    }
+                }
+            }
+            let placesQuery = PFQuery(className: "Foods")
+            
+            placesQuery.limit = 2
+            placesQuery.whereKey("objectId", notContainedIn: self.questionSeenBefore)
+            
+            placesQuery.findObjectsInBackground { (objects, error) in
+                
+                
+                if let places = objects {
+                    
+                    for place in places {
+                        
+                        self.option1.append(place["alternative1"] as! String)
+                        self.option2.append(place["alternative2"] as! String)
+                        self.option3.append(place["alternative3"] as! String)
+                        self.option4.append(place["alternative4"] as! String)
+                        self.imageFile.append(place["imageFile"] as! PFFile)
+                        self.correctAnswer.append(place["correctAlternative"] as! String)
+                        self.descriptionEng.append(place["engDescription"] as! String)
+                        self.descriptionTr.append(place["trDescription"] as! String)
+                        self.showDetail.append(false)
+                        
+                        self.tableView.reloadData()
+                        
+                        if let question = place.objectId {
+                            self.questionCompleted.append(question)
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,18 +156,339 @@ class foodsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return option1.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "foodsIdentifier", for: indexPath) as! foodsTableViewCell
-        //cell.delegate = self
-        // Configure the cell...
-
+        cell.delegate = self
+        
+        //print("Row: \(indexPath.row) showDetail: \(showDetail) Completed: \(isCompleted)")
+        
+        if (showDetail[indexPath.row] == true)/* && (detailCellRow == indexPath.row)*/ {
+            
+            //cell.placeImage.isHidden = true
+            cell.option1.isHidden = true
+            cell.option2.isHidden = true
+            cell.option3.isHidden = true
+            cell.option4.isHidden = true
+            cell.markOption1.isHidden = true
+            cell.markOption2.isHidden = true
+            cell.markOption3.isHidden = true
+            cell.markOption4.isHidden = true
+            cell.detailsButton.isHidden = false
+            cell.detailsButton.setTitle("Back", for: [])
+            
+            cell.detailTextInfo.isHidden = false
+            cell.detailTextInfo.text = descriptionEng[indexPath.row]
+            
+            imageFile[indexPath.row].getDataInBackground { (data, error) in
+                
+                if let imageData = data {
+                    
+                    if let imageToDisplay = UIImage(data: imageData) {
+                        
+                        cell.foodImage.image = imageToDisplay
+                        
+                    }
+                }
+            }
+            
+            
+        }
+        else {
+            
+            cell.detailsButton.setTitle("Details", for: [])
+            imageFile[indexPath.row].getDataInBackground { (data, error) in
+                
+                if let imageData = data {
+                    
+                    if let imageToDisplay = UIImage(data: imageData) {
+                        
+                        cell.foodImage.image = imageToDisplay
+                        
+                    }
+                }
+                
+            }
+            
+            cell.option1.isHidden = false
+            cell.option2.isHidden = false
+            cell.option3.isHidden = false
+            cell.option4.isHidden = false
+            cell.markOption1.isHidden = false
+            cell.markOption2.isHidden = false
+            cell.markOption3.isHidden = false
+            cell.markOption4.isHidden = false
+            
+            cell.detailTextInfo.isHidden = true
+            cell.option1.text = option1[indexPath.row]
+            cell.option2.text = option2[indexPath.row]
+            cell.option3.text = option3[indexPath.row]
+            cell.option4.text = option4[indexPath.row]
+            
+            
+            cell.markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+            cell.markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+            cell.markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+            cell.markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+            
+            let questionIndex = questionNo.index(of: indexPath.row)
+            
+            if let qIndex = questionIndex {
+                let answerIndex = answer[qIndex]
+                if answerIndex == 1 {
+                    cell.markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+                }
+                else if answerIndex == 2 {
+                    cell.markOption2.setImage(UIImage(named: "check.png"), for: [])
+                }
+                else if answerIndex == 3 {
+                    cell.markOption3.setImage(UIImage(named: "check.png"), for: [])
+                }
+                else if answerIndex == 4 {
+                    cell.markOption4.setImage(UIImage(named: "check.png"), for: [])
+                }
+            }
+            
+            
+            cell.detailsButton.isHidden = true
+            
+            if (isCompleted == true) {
+                
+                
+                cell.detailsButton.isHidden = false
+                cell.toDoListButton.isHidden = false
+                
+                cell.markOption1.setImage(UIImage(named: "uncheck.png"), for: [])
+                cell.markOption2.setImage(UIImage(named: "uncheck.png"), for: [])
+                cell.markOption3.setImage(UIImage(named: "uncheck.png"), for: [])
+                cell.markOption4.setImage(UIImage(named: "uncheck.png"), for: [])
+                
+                cell.markOption1.isEnabled = false
+                cell.markOption2.isEnabled = false
+                cell.markOption3.isEnabled = false
+                cell.markOption4.isEnabled = false
+                
+                let questionIndex = questionNo.index(of: indexPath.row)
+                
+                if let correctAnsInt = Int(correctAnswer[indexPath.row]) {
+                    let correctAnswerInt = correctAnsInt
+                    mustBeSelected = correctAnswerInt
+                    if let qIndex = questionIndex {
+                        selected = answer[qIndex]
+                        print("Row No: \(indexPath.row) selected: \(selected)")
+                        if answer[qIndex] == correctAnswerInt {
+                            status = 1 // correct answer
+                        }
+                        else {
+                            status = 0 // wrong answer
+                            
+                        }
+                    }
+                    else {
+                        selected = -1
+                        status = 0 // wrong answer
+                    }
+                }
+                else {
+                    status = -1 // no valid string
+                }
+                
+                if status == 1 {
+                    
+                    if selected == 1 {
+                        
+                        cell.markOption1.setImage(UIImage(named: "correct.png"), for: [])
+                        
+                    }
+                    else if selected == 2 {
+                        
+                        cell.markOption2.setImage(UIImage(named: "correct.png"), for: [])
+                        
+                    }
+                    else if selected == 3 {
+                        
+                        cell.markOption3.setImage(UIImage(named: "correct.png"), for: [])
+                        
+                    }
+                    else if selected == 4 {
+                        
+                        cell.markOption4.setImage(UIImage(named: "correct.png"), for: [])
+                        
+                    }
+                    
+                }
+                else if status == 0 {
+                    
+                    if selected == 1 {
+                        
+                        cell.markOption1.setImage(UIImage(named: "wrong.gif"), for: [])
+                        
+                    }
+                    else if selected == 2 {
+                        
+                        cell.markOption2.setImage(UIImage(named: "wrong.gif"), for: [])
+                        
+                    }
+                    else if selected == 3 {
+                        
+                        cell.markOption3.setImage(UIImage(named: "wrong.gif"), for: [])
+                        
+                    }
+                    else if selected == 4 {
+                        
+                        cell.markOption4.setImage(UIImage(named: "wrong.gif"), for: [])
+                        
+                    }
+                }
+                else {
+                    print("Unexpected")
+                    
+                }
+            }
+        }
+        
         return cell
     }
     
+
+    func didAlternativeSelected(sender: foodsTableViewCell, selectedIndex: Int){
+        let tappedIndexPath = tableView.indexPath(for: sender)
+        
+        if let selectedQuestion = tappedIndexPath?.row {
+            questionNo.append(selectedQuestion)
+        }
+        
+        answer.append(selectedIndex)
+        
+    }
+    
+    func isDetailButtonTapped(sender: foodsTableViewCell) {
+        
+        
+        /*let tappedIndexPath = tableView.indexPath(for: sender)
+         
+         if let selectedQuestion = tappedIndexPath?.row {
+         if self.descriptionEng[selectedQuestion].count > 0 {
+         detailText = self.descriptionEng[selectedQuestion]
+         print(detailText)
+         performSegue(withIdentifier: "placesDetailSegue", sender: self)
+         }
+         }*/
+        let sectionNo = 0
+        var rowNo = 1
+        
+        let tappedIndexPath = tableView.indexPath(for: sender)
+        
+        if let selectedQuestion = tappedIndexPath?.row {
+            rowNo = selectedQuestion
+        }
+        
+        let rowToSelect: IndexPath = IndexPath(row: rowNo, section: sectionNo)
+        
+        
+        if self.showDetail[rowNo] == false {
+            self.showDetail[rowNo] = true
+        }
+        else {
+            self.showDetail[rowNo] = false
+        }
+        //self.detailCellRow = rowToSelect.row
+        self.tableView.reloadRows(at: [rowToSelect], with: .fade)
+    }
+    
+    func appendToDoList(sender: foodsTableViewCell) {
+        
+        UIButton.animate(withDuration: 0.2,
+                         animations: {
+                            sender.transform = CGAffineTransform(scaleX: 0.975, y: 0.96)
+        },
+                         completion: { finish in
+                            UIButton.animate(withDuration: 0.2, animations: {
+                                sender.transform = CGAffineTransform.identity
+                            })
+        })
+        
+        let itemsObjectDescription = UserDefaults.standard.object(forKey: "toDoItemDescription")
+        
+        let itemsObjectName = UserDefaults.standard.object(forKey: "toDoItem")
+        
+        var itemsDescription = [String]()
+        
+        var itemsName = [String]()
+        
+        if let tappedIndexPath = tableView.indexPath(for: sender) {
+            
+            if let tempItemsDescription = itemsObjectDescription {
+                if let tempItemsName = itemsObjectName {
+                    itemsDescription = tempItemsDescription as! [String]
+                    itemsName = tempItemsName as! [String]
+                    
+                    itemsDescription.append(self.descriptionEng[tappedIndexPath.row])
+                    if let correctAnsInt = Int(correctAnswer[tappedIndexPath.row]) {
+                        
+                        if correctAnsInt == 1 {
+                            itemsName.append(self.option1[tappedIndexPath.row])
+                        }
+                        else if correctAnsInt == 2 {
+                            itemsName.append(self.option2[tappedIndexPath.row])
+                        }
+                        else if correctAnsInt == 3 {
+                            itemsName.append(self.option3[tappedIndexPath.row])
+                        }
+                        else if correctAnsInt == 4 {
+                            itemsName.append(self.option4[tappedIndexPath.row])
+                        }
+                    }
+                }
+                else {
+                    
+                    if let correctAnsInt = Int(correctAnswer[tappedIndexPath.row]) {
+                        itemsDescription.append(self.descriptionEng[tappedIndexPath.row])
+                        
+                        if correctAnsInt == 1 {
+                            itemsName.append(self.option1[tappedIndexPath.row])
+                        }
+                        else if correctAnsInt == 2 {
+                            itemsName.append(self.option2[tappedIndexPath.row])
+                        }
+                        else if correctAnsInt == 3 {
+                            itemsName.append(self.option3[tappedIndexPath.row])
+                        }
+                        else if correctAnsInt == 4 {
+                            itemsName.append(self.option4[tappedIndexPath.row])
+                        }
+                    }
+                }
+                
+            }
+            else {
+                itemsDescription.append(self.descriptionEng[tappedIndexPath.row])
+                
+                if let correctAnsInt = Int(correctAnswer[tappedIndexPath.row]) {
+                    if correctAnsInt == 1 {
+                        itemsName.append(self.option1[tappedIndexPath.row])
+                    }
+                    else if correctAnsInt == 2 {
+                        itemsName.append(self.option2[tappedIndexPath.row])
+                    }
+                    else if correctAnsInt == 3 {
+                        itemsName.append(self.option3[tappedIndexPath.row])
+                    }
+                    else if correctAnsInt == 4 {
+                        itemsName.append(self.option4[tappedIndexPath.row])
+                    }
+                }
+            }
+        }
+        print("itemsName : \(itemsName)")
+        print("itemsDescription: \(itemsDescription)")
+        
+        UserDefaults.standard.set(itemsName, forKey: "toDoItem")
+        UserDefaults.standard.set(itemsDescription, forKey: "toDoItemDescription")
+    }
 
     /*
     // Override to support conditional editing of the table view.
