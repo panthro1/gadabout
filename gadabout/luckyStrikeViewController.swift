a0ad184cb6c977276c97e019a025ac70615c73b8
aersoz87 <ers.ahmet@gmail.com>
2018-10-19 06:50:43 +0300

    luck strike sayfası badlands

--- gadabout/luckyStrikeViewController.swift
+++ gadabout/luckyStrikeViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class luckyStrikeViewController: UIViewController {
+    
+    
+    var option1: String = ""
+    var option2: String = ""
+    var option3: String = ""
+    var option4: String = ""
+    var descriptionEng: String = ""
+    var descriptionTr: String = ""
+    var correctAnswer : String = ""
+    var imageFile: PFFile
 
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    @IBOutlet weak var descriptionText: UITextView!
+    
     @IBAction func backTapped(_ sender: Any) {
         
         performSegue(withIdentifier: "luckyStrikeBackSegue", sender: self)
@@ -17,7 +34,40 @@ class luckyStrikeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let placesQuery = PFQuery(className: "Places")
+        
+        //print("After....")
+        //print("++++++\(self.questionSeenBefore),  \(self.questionSeenBefore.count)")
+        
+        placesQuery.limit = 2
+        placesQuery.whereKey("objectId", notContainedIn: self.questionSeenBefore)
+        
+        placesQuery.findObjectsInBackground { (objects, error) in
+            
+            
+            if let places = objects {
+                
+                for place in places {
+                    
+                    self.option1.append(place["alternative1"] as! String)
+                    self.option2.append(place["alternative2"] as! String)
+                    self.option3.append(place["alternative3"] as! String)
+                    self.option4.append(place["alternative4"] as! String)
+                    self.imageFile.append(place["imageFile"] as! PFFile)
+                    self.correctAnswer.append(place["correctAlternative"] as! String)
+                    self.descriptionEng.append(place["engDescription"] as! String)
+                    self.descriptionTr.append(place["trDescription"] as! String)
+                    
+                    
+                    if let question = place.objectId {
+                        self.questionCompleted.append(question)
+                    }
+                }
+            }
+            
+        }
+        
+    }
     }
 
     override func didReceiveMemoryWarning() {
