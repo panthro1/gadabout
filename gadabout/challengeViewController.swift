35e740674ab36ed5daea32622312d01b0189acca
aersoz87 <ers.ahmet@gmail.com>
2019-01-20 13:31:03 +0300

    Main Flow change initial

---
+++ gadabout/challengeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  challengeViewController.swift
+//  gadabout
+//
+//  Created by Ahmet on 20.01.2019.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import UIKit
+
+class challengeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
